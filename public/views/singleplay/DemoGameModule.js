f9eb63c2a540e3b7f17de97170491fd8324383ed
Vlad Orlov <orl_vlad@mail.ru>
2017-12-29 18:11:53 +0300

    Init

--- public/views/singleplay/DemoGameModule.js
+++ public/views/singleplay/DemoGameModule.js
@@ -50,6 +50,9 @@ export default class DemoGameModule {
 
         this.activeUnit = this.initiativeLine.CurrentUnit();
         GameManager.log(this.activeUnit.name + ' - let\'s start with you!');
+        GameManager.log('window.screen.width:' + window.screen.width);
+        GameManager.log('window.innerWidth:' + window.innerWidth);
+        GameManager.log('window.screen.availWidth:' + window.screen.availWidth);
         this.gameManager.unitManager.activeUnit(this.activeUnit);
         this.sendPossibleMoves();
     }
