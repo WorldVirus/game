63541283748264b8cdbb263d1e79ff27bd02ae1a
Vlad Orlov <orl_vlad@mail.ru>
2017-12-30 20:31:35 +0300

    Init

--- public/views/mainpage/mainpage.js
+++ public/views/mainpage/mainpage.js
@@ -94,15 +94,16 @@ export class MainPage extends Block {
             allButtons[i].innerHTML = `<li>${text[i]}</li>`
             allButtons[i].querySelector('li').setAttribute('value',valuePage[i])
         }
-        
+
         if (document.cookie) {
             let username = getCookie('username');
             let email = getCookie('email');
-            document.body.innerHTML += `<div style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
+            document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
             </p><a id="logout" style="margin: 4px;">Logut</a></div>`;
             document.getElementById('logout').addEventListener('click', function() {
                 deleteCookie('username');
                 deleteCookie('email');
+                document.getElementById('user-menu').remove();
                 new UserService().logout(username, email);
             });
         }
