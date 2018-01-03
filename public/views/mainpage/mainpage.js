da546987ec13d0c9d955ee9a17325a59a9ed1aac
Vlad Orlov <orl_vlad@mail.ru>
2018-01-03 17:29:23 +0300

    Init

--- public/views/mainpage/mainpage.js
+++ public/views/mainpage/mainpage.js
@@ -101,7 +101,7 @@ export class MainPage extends Block {
             credentials: 'include'
         }).then(response => {
                 if (response.status === 200) {
-                    console.log('String: ' + response.text());
+                    console.log(response.text());
                     let username = response.body.substring(response.body.indexOf('login is') + 9, response.body.length);
                     document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
                         </p><a id="logout" style="margin: 4px;">Logout</a></div>`;
