010b01c8e55b7253998e4e3ebd94a679a59afac6
Vlad Orlov <orl_vlad@mail.ru>
2018-01-03 18:43:58 +0300

    Init

--- public/views/main.js
+++ public/views/main.js
@@ -34,6 +34,13 @@ wrapper.appendChildBlock('menu',new Block('div',['menu']))
             .then(() => window.history.pushState({}, '', '/game'))
             .then(() => {
                 wrapper.appendChildBlock('name',new Block('div',['user']).setText( setter(formdata[0])));
+                let username = formdata[0];
+                document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
+                            </p><a id="logout" style="margin: 4px;">Logout</a></div>`;
+                document.getElementById('logout').addEventListener('click', function () {
+                    document.getElementById('user-menu').remove();
+                    new UserService().logout();
+                });
             })
             .then (() => new Mediator().publish('VIEW_LOAD'))
             .catch(error => {
@@ -55,6 +62,13 @@ wrapper.appendChildBlock('menu',new Block('div',['menu']))
              .then(() => window.history.pushState({}, '', '/game'))
               .then(() => {
                   wrapper.appendChildBlock('name',new Block('div',['user']).setText( setter(formdata[0])));
+                  let username = formdata[0];
+                  document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
+                            </p><a id="logout" style="margin: 4px;">Logout</a></div>`;
+                  document.getElementById('logout').addEventListener('click', function () {
+                      document.getElementById('user-menu').remove();
+                      new UserService().logout();
+                  });
              }).catch(error => {
                  return error.text();
             }).then(data => {
