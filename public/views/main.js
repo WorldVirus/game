71766ebca18bd69074d6969fadc08d912c66d6ad
Vlad Orlov <orl_vlad@mail.ru>
2018-01-03 21:47:35 +0300

    Init

--- public/views/main.js
+++ public/views/main.js
@@ -37,8 +37,7 @@ wrapper.appendChildBlock('menu',new Block('div',['menu']))
             .then(() => {
                 if (!document.querySelector('.user-menu')) {
                     let username = formdata[0];
-                    document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
-                            </p><a id="logout" style="margin: 4px;">Logout</a></div>`;
+                    document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;background-color: #83c0f6;border-radius: 5px;"><p style="margin: 4px;">${username}</p><a id="logout" style="margin: 4px;background-color: #51ecec;padding: 4px;display: block;text-align: center;border-radius: 5px;border: 2px solid black;">Logout</a></div>`;
                     document.getElementById('logout').addEventListener('click', function () {
                         document.getElementById('user-menu').remove();
                         new UserService().logout();
@@ -68,8 +67,7 @@ wrapper.appendChildBlock('menu',new Block('div',['menu']))
               .then(() => {
                   if (!document.querySelector('.user-menu')) {
                       let username = formdata[0];
-                      document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;"><p style="margin: 4px;">${username}
-                            </p><a id="logout" style="margin: 4px;">Logout</a></div>`;
+                      document.body.innerHTML += `<div id="user-menu" style="position:absolute;top: 0;  background: white;right: 0;background-color: #83c0f6;border-radius: 5px;"><p style="margin: 4px;">${username}</p><a id="logout" style="margin: 4px;background-color: #51ecec;padding: 4px;display: block;text-align: center;border-radius: 5px;border: 2px solid black;">Logout</a></div>`;
                       document.getElementById('logout').addEventListener('click', function () {
                           document.getElementById('user-menu').remove();
                           new UserService().logout();
